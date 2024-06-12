f341756f764cad980884297357a58a48996b06a4
Rahulkr00 <care.rahulsingh07022003@gmail.com>
2024-06-12 19:22:41 +0530

    first javascript

---
+++ variables.js
@@ -0,0 +1,14 @@
+let productname = "Iphone 15 pro Max";
+const productprice = 150000;
+var Company ="Apple"
+
+
+
+
+console.log(productname, productprice, Company);
+
+console.log("Name of the Product is;" , productname);
+
+console.log("Price of the Product;" , productprice);
+
+console.log("Company of the Product;" , Company);
