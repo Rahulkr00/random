cfe175c5d32450f49b9a01db76997302c442b571
Rahulkr00 <care.rahulsingh07022003@gmail.com>
2024-06-12 19:07:26 +0530

    this is real

---
+++ string.js
@@ -0,0 +1,24 @@
+let singlequotestring = 'Hello World'
+
+let doublequotestring = "Hello World I am Rahul "
+
+let backtickquotestring = `Hello Back World`
+
+console.log(singlequotestring, doublequotestring, backtickquotestring); 
+
+
+let greeting = "Hello I'm Rahul"
+
+console.log(greeting);
+
+
+let saying = 'My Friend Said: "How are You Doing" ';
+
+console.log(saying)
+
+let multilinestring = `Hello Friends
+My Name
+is 
+Rahul Kumar`
+
+console.log(multilinestring);
